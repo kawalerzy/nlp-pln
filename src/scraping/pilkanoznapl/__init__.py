c5b120b914879d3e7aa8698186fe9068a1d869ed
Jakub Sokolowski <jakub.sokolowski1996@gmail.com>
2020-02-06 23:21:42 +0100

    add constants for categories and page limits

--- src/scraping/pilkanoznapl/__init__.py
+++ src/scraping/pilkanoznapl/__init__.py
@@ -1,3 +1,19 @@
 MAX_PAGE_NUM = 1899
 base_url = 'https://pilkanozna.pl'
 columns = ['link', 'title', 'category', 'date']
+
+categories = {
+    64: "ekstraklasa",
+    65: "I_liga",
+    66: "reprezentacja",
+    117: "bundesliga",
+    134: "premier_league"
+}
+
+page_limits = {
+    64: 1891,
+    65: 244,
+    66: 296,
+    117: 365,
+    134: 721
+}
