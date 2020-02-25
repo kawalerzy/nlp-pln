950f0ba0a2585a095bf1a5144cdce784ea22f5e5
Jakub Sokołowski <jakub.sokolowski1996@gmail.com>
2020-02-25 22:16:14 +0100

    add loader for credentials

--- src/config/__init__.py
+++ src/config/__init__.py
@@ -1,14 +1,52 @@
+import json
+import operator
 import os
+from functools import reduce
 from pathlib import Path
 
 CONFIG_DIR = os.path.dirname(os.path.abspath(__file__))
 ROOT_PATH = str(Path(CONFIG_DIR).parents[1])
+DATA_PATH = '{}/data'.format(ROOT_PATH)
+PAGES_PATH = '{}/pages'.format(ROOT_PATH)
 
-DATA_PATH = '{}/data'.format(str(Path(CONFIG_DIR).parents[1]))
-PAGES_PATH = '{}/pages'.format(str(Path(CONFIG_DIR).parents[1]))
 
-CELERY_BROKER_URL = os.getenv('CELERY_BROKER_URL', 'secretconnstr')
+class ConfigLoader:
+    def __init__(self):
+        self.auth_file = os.path.join(ROOT_PATH, 'auth.json')
+        self.config = {}
+        self.__load__()
+
+    def get(self, path: str, default=""):
+        try:
+            return reduce(operator.getitem, path.split('.'), self.config)
+        except KeyError:
+            return default
+
+    def __load__(self):
+        with open(self.auth_file, 'r') as conf:
+            self.config = json.loads(conf.read())
+
+
+loader = ConfigLoader()
+
+CELERY_BROKER_URL = loader.get('CELERY.BROKER_URL', 'redis://localhost:6379')
 CELERY_RESULT_BACKEND = CELERY_BROKER_URL
-CELERY_ACCEPT_CONTENT = ['json']
-CELERY_TASK_SERIALIZER = 'json'
-CELERY_RESULT_SERIALIZER = 'json'
+
+PSYCOPG2_CONN_STR = "host={} port={} dbname={} user={} password={}".format(
+    loader.get('DB.HOST'),
+    loader.get('DB.PORT'),
+    loader.get('DB.NAME'),
+    loader.get('DB.USER'),
+    loader.get('DB.PASSWORD')
+)
+
+DEFAULT_SQLALCHEMY_ENGINE_STR = "postgresql+psycopg2://{}:{}@{}:{}/{}".format(
+    loader.get('DB.USER'),
+    loader.get('DB.PASSWORD'),
+    loader.get('DB.HOST'),
+    loader.get('DB.PORT'),
+    loader.get('DB.NAME')
+)
+
+SQLALCHEMY_ENGINE_STR = DEFAULT_SQLALCHEMY_ENGINE_STR
+
